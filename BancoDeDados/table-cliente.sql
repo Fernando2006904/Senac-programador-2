56c7ae750a744c8f36a926e480b852f07a1761b8
FERNANDO LIMA ALVES <fernando.lalves2@senacsp.edu.br>
2025-04-10 22:21:14 -0300

    Novos Dados

--- BancoDeDados/table-cliente.sql
+++ BancoDeDados/table-cliente.sql
@@ -7,10 +7,18 @@ data_nascimento date NOT NULL,
 telefone VARCHAR(11) NOT NULL UNIQUE,
 email VARCHAR(25) NOT NULL UNIQUE,
 genero INT NOT NULL,
-etnia INT NOT NULL,
 estrangeiro BIT NOT NULL,
-tipo INT NOT NULL
+tipo INT NOT NULL,
+id_endereço INT NOT NULL,
+FOREIGN KEY (id_endereco)
+REFERENCES endereco (id) 
+
+
+
 
 );
 
-
+INSERT INTO cliente (nome, data_nascimento, telefone, email, genero, estrangeiro, tipo, id_endereço) VALUES
+('Fernando', '1990-01-01', '11999999999', 'Fefe18++@gmail.com', 'Masculino',  1, 0, 0, (SELECT id FROM endereço WHERE logradouro = 'endereçoFernando')),
+('Valdeci', '1990-01-01', '119999999777', 'Valdeci14!@gmail.com', 'Masculino',  1, 0, 0, (SELECT id FROM endereço WHERE logradouro = 'endereçoValdeci')),
+('Maria', '1990-01-01', '11999999988', 'Maria10*@gmail.com', 'Feminino',  1, 0, 0, (SELECT id FROM endereço WHERE logradouro = 'endereçoMaria'));
--- BancoDeDados/table-endereço.sql
+++ BancoDeDados/table-endereço.sql
@@ -12,6 +12,10 @@ cep VARCHAR(8) NOT NULL
 
 
 
+INSERT INTO endereço (logradouro, numero, complemento, bairro, municipio, estado, cep) VALUES 
+('endereço Fernando', '91', 'ap10', 'Moraes', 'Embu Guaçu', 'Maranhão', '12345-10', NULL),
+('endereço Valdeci ', '76', 'Ap20', 'Jardim Espirito Santo', 'Vargem Grande', 'Amazonas', '12345-20', NULL),
+('endereço Maria ', '10', 'Ap30', 'Jardim Santa Cruz', 'Embu4', 'Rio de Janeiro', '12345-30', NULL);
 
 
-
+SELECT * FROM endereço;
